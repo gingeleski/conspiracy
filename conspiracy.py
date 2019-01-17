3f160a3e3184b74fa5482939a1bb262df9119a52
Randy Gingeleski <rjg26247@gmail.com>
2019-01-16 22:39:03 -0500

    start #14

---
+++ conspiracy.py
@@ -0,0 +1,55 @@
+"""
+conspiracy.py
+
+Conspiracy - automated web app hacking
+
+"""
+
+import argparse
+
+# Instantiate the argument parser
+parser = argparse.ArgumentParser(description='Conspiracy - automated web app hacking')
+
+parser.add_argument('target', type=str, help='Overall target URL')
+parser.add_argument('--hitlist', type=str, help='Optional path to text file of URLs to analyze')
+args = parser.parse_args()
+
+inscope_urls = {}    # (sub)domains in scope
+hitlist = []         # optional individual urls to specially hit
+
+requested_items = {} # keeps track of every unique request
+
+print(args.target)
+print(args.hitlist)
+
+# add the overall target to inscope_urls
+
+# was hitlist flag specified?
+
+# is the given path valid for a file?
+
+#     read it in as a text file
+
+#     break down by lines
+
+#     add each item to the hitlist
+
+#     also add root url to `inscope_urls` if not already in there
+
+# for each item in hitlist
+
+#     request it in headless chrome, proxied through burp suite
+
+#     wait for it to load
+
+#     (add some hooks and handles at this point for future functionality)
+
+#     close the tab or whatever
+
+# for each item in `inscope_urls`
+
+#     scan with burp (maybe just crawl by default)
+
+#     run sslyze
+
+# print out all findings for user
