fa1af0d00516ee4e044723675bd045b31d834925
Randy Gingeleski <rjg26247@gmail.com>
2019-01-17 18:34:10 -0500

    #14 frame logical loops

--- conspiracy.py
+++ conspiracy.py
@@ -22,34 +22,33 @@ requested_items = {} # keeps track of every unique request
 print(args.target)
 print(args.hitlist)
 
+output = ''
+
 # add the overall target to inscope_urls
+inscope_urls[args.target] = True
 
 # was hitlist flag specified?
-
-# is the given path valid for a file?
-
-#     read it in as a text file
-
-#     break down by lines
-
-#     add each item to the hitlist
-
-#     also add root url to `inscope_urls` if not already in there
+if args.hitlist != None:
+    pass
+    # is the given path valid for a file?
+    #     read it in as a text file
+    #     break down by lines
+    #     add each item to the hitlist
+    #     also add root url to `inscope_urls` if not already in there
 
 # for each item in hitlist
-
-#     request it in headless chrome, proxied through burp suite
-
-#     wait for it to load
-
-#     (add some hooks and handles at this point for future functionality)
-
-#     close the tab or whatever
+for item in hitlist:
+    pass
+    #     request it in headless chrome, proxied through burp suite
+    #     wait for it to load
+    #     (add some hooks and handles at this point for future functionality)
+    #     close the tab or whatever
 
 # for each item in `inscope_urls`
-
-#     scan with burp (maybe just crawl by default)
-
-#     run sslyze
+for inscope_url, _ in inscope_urls.items():
+    pass
+    #     scan with burp (maybe just crawl by default)
+    #     run sslyze
 
 # print out all findings for user
+print(output)
