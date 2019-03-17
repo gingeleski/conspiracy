9a670cf303c04728ec3f9f427b03ef6adfbac667
Randy Gingeleski <rjg26247@gmail.com>
2019-03-17 12:15:16 -0400

    #39 hmmmmmmm 1

--- conspiracy.py
+++ conspiracy.py
@@ -1,20 +1,57 @@
 import asyncio
-from pyppeteer import launch
+import pyppeteer
+
+BROWSER_PAGE_PLUGINS = []
 
 hitlist = ['http://example.com', 'https://play.google.com/store', 'https://play.google.com/store/apps']
 
-async def browser_stuff():
-    global hitlist
-    browser = await launch()
-    for url in hitlist:
+async def get_browser(use_burp_suite_proxy):
+    """
+    Get browser
+
+    Params:
+        use_burp_suite_proxy (bool)
+    """
+    if use_burp_suite_proxy:
+        return await pyppeteer.launch(headless=True,args=['--proxy-server=' + BURP_SUITE_PROXY])
+    # else, don't proxy
+    return await pyppeteer.launch(headless=True)
+
+async def run_processing_on_hitlist(use_burp_suite_proxy):
+    """
+    Run processing on hitlist
+
+    Params:
+        use_burp_suite_proxy (bool)
+    """
+    global BROWSER_PAGE_PLUGINS
+    global hitlist #, logger
+    # We're going to request stuff with headless Chrome, proxied through Burp Suite
+    browser = await get_browser(use_burp_suite_proxy)
+    # Then for each item (URL) ...
+    for item in hitlist:
+        #logger.info(f'Now requesting {item.strip()}')
+        # Request the page
         page = await browser.newPage()
-        await page.goto(url)
-        await page.screenshot({'path': 'example_' + url.replace('/', '').replace(':', '') + '.png'})
+        try:
+            await page.goto(item)
+            await page.screenshot({'path': 'example_' + item.replace('/', '').replace(':', '') + '.png'})
+        except pyppeteer.errors.TimeoutError as e:
+            print(e)
+        #    logger.warning('Timed out on request to ' + item)
+        #    logger.warning('\t' + str(e))
+        # Looping through plugins of this type
+        for plugin in BROWSER_PAGE_PLUGINS:
+            logger.info('Begin plugin: ' + plugin.get_name() + ' <' + item.strip() + '>')
+            plugin.executePerDomainAction(inscope_url)
+            logger.info('End plugin: ' + plugin.get_name() + ' <' + item.strip() + '>')
+        # Close the page now
+        await page.close()
     await browser.close()
 
 def main():
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(browser_stuff())
+    result = loop.run_until_complete(run_processing_on_hitlist(False))
 
 if __name__ == '__main__':
     main()
