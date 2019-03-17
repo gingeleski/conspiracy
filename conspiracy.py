3111eeaf7e443bd56e67e452b7364cae3bf36927
Randy Gingeleski <rjg26247@gmail.com>
2019-03-17 12:06:18 -0400

    #39 iterating over global url list non-issue

--- conspiracy.py
+++ conspiracy.py
@@ -1,11 +1,15 @@
 import asyncio
 from pyppeteer import launch
 
+hitlist = ['http://example.com', 'https://play.google.com/store', 'https://play.google.com/store/apps']
+
 async def browser_stuff():
+    global hitlist
     browser = await launch()
-    page = await browser.newPage()
-    await page.goto('http://example.com')
-    await page.screenshot({'path': 'example.png'})
+    for url in hitlist:
+        page = await browser.newPage()
+        await page.goto(url)
+        await page.screenshot({'path': 'example_' + url.replace('/', '').replace(':', '') + '.png'})
     await browser.close()
 
 def main():
